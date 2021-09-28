f3024f3b48b9437aabe020c2d3e79cf792058bd7
sanskarpareek14 <pareek081@gmail.com>
2021-09-28 18:56:02 +0530

    primitive-datatypes

---
+++ app.js
@@ -0,0 +1,12 @@
+//String
+console.log(typeof "Hello world") //returns the type of variable
+console.log(typeof 'd')
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof 1.2222)
+console.log(typeof -11)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
