ad0d2789776d83b65d92427278bbb9ab53750a4a
sanskarpareek14 <pareek081@gmail.com>
2021-09-28 19:38:13 +0530

    Operators

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
-//String
-console.log(typeof "Hello world") //returns the type of variable
-console.log(typeof 'd')
+// var arr = [1, 2, 3, 4]
+// console.log(arr)
+// console.log(arr.length)
+// arr.push(5)
+// console.log(arr)
 
-//Numbers
-console.log(typeof 1)
-console.log(typeof 1.2222)
-console.log(typeof -11)
+// const age = 19
+// console.log(age)
 
-//boolean
-console.log(typeof true)
-console.log(typeof false)
+//Operators
+
+//Arthmetic Operators
+var a = 2
+var b = 10
+console.log('a + b=', a + b)
+console.log('a + b=', a + b)
+console.log('a + b=', a + b)
+console.log('a + b=', a + b)
+console.log('a + b', a + b)
+console.log('a + b', a + b)
+console.log('b%a=', b % a)
+
+// ASSIGNMENT Operators
+b = 11
+console.log('b = ', b)
+b += 1 //b++ 
+console.log('b = ', b)
+b--
+console.log('b =', b)
+
+//COMPARISON Operators
+var c = 10
+var d = 2
+var f = '10'
+console.log(c > d)
+console.log(c == d)
+console.log(c < d)
+console.warn(c == f)
+console.warn(c === f)
+
+console.log(c + f)
+
+//LOGICAL Operators
+
+console.log('true && true', true && true)
+console.log('true && false', true && false)
+console.log('false && true', true && true)
+console.log('false && true', false && true)
+
+console.log('true || true', true || true)
+console.log('true || false', true || false)
+console.log('false || true', true || true)
+console.log('false || true', false || true)
