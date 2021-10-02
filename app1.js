fb5e6c9e31e38ea0f4ca57fcf83c06e3d4fba7ba
sanskarpareek14 <pareek081@gmail.com>
2021-10-02 16:09:09 +0530

    splice

--- app1.js
+++ app1.js
@@ -1,14 +1,4 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
-var new_arr = arr.filter((element, index) => {
-    if (element > 2) {
-        return true
-    }
-})
-console.log(new_arr)
-// for (var i = 0; i < arr.length; i++) {
-//     if (arr[i] % 2 == 0) {
-//         new_arr.push(arr[i])
-//     }
-// }
-// console.log(new_arr)
+arr.splice(3, 1)
+console.log(arr)
