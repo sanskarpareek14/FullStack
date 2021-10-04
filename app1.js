cb5addec269f4a9f8f72b3401c3286dbc6c57c70
sanskarpareek14 <pareek081@gmail.com>
2021-10-04 19:31:06 +0530

    Objects

--- app1.js
+++ app1.js
@@ -1,30 +1,12 @@
-var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
-
-var chores = []
-
-var deleteItem = (value) => {
-    const index = chores.indexOf(value)
-    chores.splice(index, 1)
-    console.log(chores)
-}
-
-
-const callbackFunction = (mystery) => {
-    const inputvalue = input.value
-    if (chores.includes(inputvalue)) {
-        console.log('already exists')
-    } else {
-        chores.push(inputvalue)
-        const element = document.createElement('li')
-        const textnode = document.createTextNode(inputvalue)
-        element.appendChild(textnode)
-        list.appendChild(element)
-        element.addEventListener('click', (event) => {
-            deleteItem(event.target.innerHTML)
-        })
-
+let car = {
+    Name: "C Class",
+    Manufacturer: "Mercedes",
+    print: () => {
+        console.log(this)
+        console.log(`${car.Name} was created by ${car.Manufacturer}`)
     }
 }
-button.addEventListener('click', callbackFunction)
+
+console.log(Object)
+
+console.log(car.hasOwnProperty('Manufacturer'))
