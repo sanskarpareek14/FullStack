cc53dbcc2d235281cb3722afe29e7a187fc2256a
sanskarpareek14 <pareek081@gmail.com>
2021-10-04 19:52:55 +0530

    fetch

--- app1.js
+++ app1.js
@@ -1,12 +1,3 @@
-let car = {
-    Name: "C Class",
-    Manufacturer: "Mercedes",
-    print: () => {
-        console.log(this)
-        console.log(`${car.Name} was created by ${car.Manufacturer}`)
-    }
-}
+var data = fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-console.log(Object)
-
-console.log(car.hasOwnProperty('Manufacturer'))
+console.log(data)
