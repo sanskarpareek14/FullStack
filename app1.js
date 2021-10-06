f46f11d51144a3e440f48026eb596252a89db1ab
sanskarpareek14 <pareek081@gmail.com>
2021-10-06 19:28:00 +0530

    async await

--- app1.js
+++ app1.js
@@ -1,21 +1,17 @@
-const Table = document.querySelector('table')
 
-const createRow = (user) => {
-    let newRow = document.createElement('tr')
-    let element = `
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>${user.username}</td>
-            `
-    newRow.innerHTML = element
-    return newRow
+// fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//         data.forEach(user => {
+//             console.log(data)
+//         })
+//     })
+const fetchData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    console.log(response)
+
+    const data = await response.json()
+    console.log(data)
 }
 
-fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
-    .then(data => {
-        console.log(data)
-        data.forEach(user => {
-            Table.appendChild(createRow(user))
-        })
-    })
+fetchData()
