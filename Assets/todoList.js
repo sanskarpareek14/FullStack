ed59b9b12efabe3b4aa7fae88862b8baee094c06
sanskarpareek14 <pareek081@gmail.com>
2021-10-07 17:56:39 +0530

    TODO LIST deletion items successful added

--- Assets/todoList.js
+++ Assets/todoList.js
@@ -25,15 +25,21 @@ const callbackFunction = () => {
         chores.push(inputvalue)
         const element = document.createElement('li')
         const delete_button = document.createElement('div')
+        // const hamburger_button = document.createElement('div')
         element.style = "display:flex;align-items: center; justify-content: space-between; margin: 6px 8px 10px 2px;padding: 5px 5px 5px 10px;background-color: white;height: 35px;border-radius: 5px;"
         delete_button.innerHTML = "&#x2715;"
-        delete_button.style = "cursor:pointer;"
+        delete_button.style = "cursor:pointer; color:hover:crimson;"
+        // hamburger_button.innerHTML = "&#9776";
+        // hamburger_button.style = "cursor:pointer; margin-left: 4px;"
         const textnode = document.createTextNode(inputvalue)
         delete_button.addEventListener('click', (event) => {
             element.remove(event.target)
+            chores.pop(chores.indexOf(event.target.value))
+            count.innerHTML = `${chores.length} items`
         })
         element.appendChild(textnode)
         element.appendChild(delete_button)
+        // element.appendChild(hamburger_button)
         list.appendChild(element)
         count.innerHTML = `${chores.length} items`
     }
