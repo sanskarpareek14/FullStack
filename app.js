d64bd3ea65c3bdc4776321d1bd6835348d8697fa
sanskarpareek14 <pareek081@gmail.com>
2021-10-11 19:30:10 +0530

    append in file using write file and readFIleSync

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 
-fs.writeFile('./test.txt', 'New stuff inside', (error, data) => {
+// fs.writeFile('./test.txt', 'New stuff inside', (error) => {
+//     if (error) console.log(error)
+// })
+fs.writeFile('./test.txt', `Append me  ${fs.readFileSync('./test.txt', 'utf8')}`, (error) => {
     if (error) console.log(error)
 })
