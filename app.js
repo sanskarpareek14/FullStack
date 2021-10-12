8f5b91fe2deed6f608c9855e5464cb3518946303
sanskarpareek14 <pareek081@gmail.com>
2021-10-12 21:43:21 +0530

    npm

--- app.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-// fs.writeFile('./test.txt', 'New stuff inside', (error) => {
-//     if (error) console.log(error)
-// })
-fs.writeFile('./test.txt', `Append me  ${fs.readFileSync('./test.txt', 'utf8')}`, (error) => {
-    if (error) console.log(error)
-})
