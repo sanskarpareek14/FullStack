2855db65e5bed1ec0a4bbf606c92761172f90595
sanskarpareek14 <pareek081@gmail.com>
2021-10-13 18:48:21 +0530

    Server http

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log('A message')
+// const http = require('http')
+// const fs = require('fs')
+// const path = require('path')
+// const { url } = require('inspector')
+// console.log(http)
+
+// const server = http.createServer((request, response) => {
+//     // console.log(request.url)
+//     // console.log(request.method)
+//     // const path = request.url
+//     // const markup = fs.readFileSync(path.resolve('./index.html'))
+//     // response.write(markup)
+//     // response.end()
+//     const { url } = request
+//     if (url == '/login') {
+//         response.write(`<h1>Login</h1>`)
+//     }
+//     if (url == '/signup') {
+//         response.write(`<h1>Signup</h1>`)
+//     }
+// })
+
+// server.listen(3000, () => {
+//     console.log(`Server runs at port ${3000}`)
+// })
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req)
+})
+
+app.listen(3000, () => {
+    console.log('Server listening at port 3000')
+})
