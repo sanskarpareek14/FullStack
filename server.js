3b9e64aab0be634fa44eab821100842df9692060
sanskarpareek14 <pareek081@gmail.com>
2021-10-14 18:00:47 +0530

    middleware function and next()

--- server.js
+++ server.js
@@ -2,24 +2,16 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('GET')
+const verify = (req, res, next) => {
+    if (req.headers['user-agent'] === 'Thunder Client (https://www.thunderclient.io)') next()
+    else res.send('BLOCKED')
+}
+
+app.get('/', verify, (req, res) => {
+    res.send('Verified')
 })
-app.post('/', (req, res) => {
-    res.send('POST')
-})
-app.put('/', (req, res) => {
-    res.send('PUT')
-})
-app.delete('/', (req, res) => {
-    res.send('DELETE')
-})
-app.patch('/', (req, res) => {
-    res.send('PATCH')
-})
-app.get('/login', (req, res) => {
-    res.send('login form')
-})
+
+
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
 })
