4ccaa673ab85001cacb89fda8951a8259291e188
sanskarpareek14 <pareek081@gmail.com>
2021-10-14 17:31:15 +0530

    Different http request

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log('A message')
+const express = require('express')
+const PORT = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('GET')
+})
+app.post('/', (req, res) => {
+    res.send('POST')
+})
+app.put('/', (req, res) => {
+    res.send('PUT')
+})
+app.delete('/', (req, res) => {
+    res.send('DELETE')
+})
+app.patch('/', (req, res) => {
+    res.send('PATCH')
+})
+app.get('/login', (req, res) => {
+    res.send('login form')
+})
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
+})
