89ebd98b425d16f49becb523c89af495d6b3c33c
sanskarpareek14 <pareek081@gmail.com>
2021-10-14 20:24:09 +0530

    express.json and post request

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 const express = require('express')
 const PORT = 3000
 const app = express()
-
-const isVerified = (req, res, next) => {
-    if (req.headers.admin === 'true') next()
-    else res.send('UNAUTHORISED')
-}
-
-app.get('/public', (req, res) => {
-    console.log(req.headers)
-    res.send('I am a public route')
+app.use(express.json())
+app.post('/signup', (req, res) => {
+    console.log(req.body)
+    res.send('dadada')
 })
-app.get('/private', isVerified, (req, res) => {
-    res.send('I am a admin route')
-})
-
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
