8f762845a443e49eb960b78fa9b9a05e8b14161e
sanskarpareek14 <pareek081@gmail.com>
2021-10-17 13:19:05 +0530

    Routes,Schema, Databse

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const PORT = 3000
+const database = require('./database/db')
+PORT = 3001
 const app = express()
 app.use(express.json())
-app.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send('dadada')
-})
+
 
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
+    console.log(`Server listening at port ${PORT}`)
 })
